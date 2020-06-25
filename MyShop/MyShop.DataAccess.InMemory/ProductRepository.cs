e9298d204ddfbb3f3a18c7fa6cbcbc8bfa058f76
Trendon Cato <electric5388@hotmail.com>
2020-06-26 03:36:30 +0800

    Product Explorer Built And Tested

--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using MyShop.Core.Model;
 
 namespace MyShop.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
 
