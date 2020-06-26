61cdb52083557374b09159f15e1bdac0113162b2
Trendon Cato <electric5388@hotmail.com>
2020-06-26 14:16:46 +0800

    Product Grouping added

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Model;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -12,10 +13,14 @@ namespace MyShop.WebUI.Controllers
     {
         ProductRepository context;
 
+        ProductCategoryRepository productCategories;
+
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
+
         // GET: ProductManager
         public ActionResult Index()
         {
@@ -25,8 +30,11 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -55,7 +63,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
 
