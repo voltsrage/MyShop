0b26c5fa844b3a8f4c7b3406c771b2df20db8c44
Trendon Cato <electric5388@hotmail.com>
2020-06-27 23:07:01 +0800

    Secured Admin

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -9,6 +9,7 @@ using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductCategoryManagerController : Controller
     {
         IRepository<ProductCategory>  context;
